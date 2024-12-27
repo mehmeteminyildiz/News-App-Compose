320fa244c5d502e409934caa153fafaba4e2b12b
mehmeteminyildiz <mhmtxyildiz@gmail.com>
2024-12-27 13:58:19 +0300

    feat: Splash api integrated

--- app/src/main/java/com/mey/newsapp/MainActivity.kt
+++ app/src/main/java/com/mey/newsapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mey.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mey.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsAppTheme {
 
